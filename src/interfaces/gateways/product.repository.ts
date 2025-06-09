be268ca5254e7983c786c0fa4eebbb173c005768
MAC <mehdighachoui@gmail.com>
2025-06-09 23:11:24 +0200

    clean-arch

--- src/interfaces/gateways/product.repository.ts
+++
@@ -1,8 +0,0 @@
-import { Product } from "../../entities/product";
-
-export interface IProductRepository {
-  findProducts(): Promise<Product[]>;
-  findProductById(id: number): Promise<Product | null>;
-  addProduct(product: Product): Promise<void>;
-  updateProduct(product: Product): Promise<void>;
-}
